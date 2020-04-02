176d34a06163bb8942e6d863ee2c1bd7841f14c1
Gulya <osx@192.168.1.7>
2020-04-02 15:21:00 +0300

    intr

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("Hello World")
+my_age = 20
+half_my_age = 12.5
+greeting = "Hello World!"
+name = "My name is Pink."
+greeting_with_name = greeting + "" + name
+print(greeting_with_name + " " + "I am" + " " + str(my_age) + " " "years old! I am  married to a handsome man!")
