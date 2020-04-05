9bed6b12f2d22cced17812a05bd0fea1cfbd74f7
pinkblack <gulchaman.keneshbekovna@gmail.com>
2020-04-05 11:43:10 +0300

    Function

---
+++ return_function.py
@@ -0,0 +1,7 @@
+def square_point(x_value,y_value):
+  x_2 = x_value * x_value 
+  y_2 = y_value * y_value
+  return x_2, y_2
+x_squared, y_squared = square_point(1,3)
+print(x_squared)
+print(y_squared)
