2cc3f26885269c302ab64f1f6329ce2b7c3d2c41
pinkblack <gulchaman.keneshbekovna@gmail.com>
2020-04-06 20:26:37 +0300

    Findvolume

---
+++ review2.py
@@ -0,0 +1,10 @@
+def findvolume(length = 1, width = 1, depth = 1):
+    print("length =" +str(length))
+    print("width = " + str(width))
+    print("depth = " + str(depth))
+    return length * width * depth
+findvolume(1,2,3)
+findvolume(length = 1, depth = 2, width = 1)
+# findvolume(1, 1, 2)
+
+
