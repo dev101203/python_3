53bc4168ed570546094c1e262342b17337de4738
pinkblack <gulchaman.keneshbekovna@gmail.com>
2020-04-19 11:27:34 +0300

    BMI

---
+++ mybmi.py
@@ -0,0 +1,14 @@
+name = 'Aizh'
+height = 168
+weight = 54
+
+bmi = weight / (height ** 168)
+print('bmi: ')
+print(bmi)
+
+if bmi > 56:
+  print(name)
+  print("is not overweight")
+else:
+  print(name)
+  print('overweight')
