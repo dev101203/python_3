143b98b5e3e3163ffe8b0ba064e72d2189cdae97
pinkblack <gulchaman.keneshbekovna@gmail.com>
2020-04-20 21:35:45 +0300

    lists_comprehensions

---
+++ loop.py
@@ -0,0 +1,25 @@
+#create for loop 
+board_games = ['Settlers of Catan', 'Carcassone', 'Power Grid', 'Agricola', 'Scrabble']
+
+sport_games = ['football', 'football - American', 'hockey', 'baseball', 'cricket']
+
+for game in board_games:
+  print(game)
+
+for sport in sport_games:
+  print("Name of sport game: " + sport)
+#
+for i in range(3):
+  print("WARNING!")
+
+#while 
+all_students = ["Alex", "Briana", "Cheri", "Daniele", "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
+students_in_poetry = []
+
+while len(students_in_poetry) < 6:
+  student = all_students.pop()
+  students_in_poetry.append(student)
+  
+print(students_in_poetry)
+
+
