e465fc504ef7bad4e69f13904d251a47781e6c98
pinkblack <gulchaman.keneshbekovna@gmail.com>
2020-04-21 12:47:01 +0300

    review

---
+++ loop_review.py
@@ -0,0 +1,23 @@
+single_digits = [0,1,2,3,4,5,6,7,8,9]
+
+squares = []
+for square in single_digits:
+  squares.append(square**2)
+print(squares)
+
+cubes =[]
+for cube in single_digits:
+  cubes.append(cube**3)
+print(cubes)
+
+
+print(single_digits[0])
+print(single_digits[1])
+print(single_digits[2])
+print(single_digits[3])
+print(single_digits[4])
+print(single_digits[5])
+print(single_digits[6])
+print(single_digits[7])
+print(single_digits[8])
+print(single_digits[9])
