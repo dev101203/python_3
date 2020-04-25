f12b3c0582532abd0f996b50935662cfbc0166fd
Gulya Abdy <gulyaabdy@192.168.1.7>
2020-04-25 20:48:00 +0300

    String

---
+++ string.py
@@ -0,0 +1,31 @@
+#iterating through strings
+def get_length(word):
+  counter = 0
+  for letter in word:
+    counter = counter + 1 
+  return counter
+print(get_length('blue'))
+
+#escape
+password = "theycallme \" crazy\" 91"
+print(password)
+
+#negative indices
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+second_to_last = company_motto[-2]
+
+final_word = company_motto[-4:]
+print(second_to_last,final_word)
+
+#slicing
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+  temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return temp_password
+
+temp_password = password_generator(first_name, last_name)
+
+print(temp_password)
